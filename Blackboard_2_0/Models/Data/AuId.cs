cc50845c447e6ebed4fe881c6486523c5816ff56
Mathias <math084m@hotmail.com>
2019-04-04 12:55:03 +0200

    Fix teachers in DB

--- Blackboard_2_0/Models/Data/AuId.cs
+++ Blackboard_2_0/Models/Data/AuId.cs
@@ -10,5 +10,6 @@ namespace Blackboard_2_0.Models.Data
         public int Id { get; set; }
         public string Role { get; set; }
         public Student Student { get; set; }
+        public Teacher Teacher { get; set; }
     }
 }

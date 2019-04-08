41672ca25dd6211dee89203c44477d850620aad0
Oskarthansen <oskarthansen@outlook.dk>
2019-04-08 11:53:44 +0200

    Calendar controller

---
+++ Blackboard_2_0/Controllers/CalendarsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Blackboard_2_0.Models.Data;
+
+namespace Blackboard_2_0.Controllers
+{
+    public class CalendarsController : Controller
+    {
+        private readonly BbContext _context;
+
+        public CalendarsController(BbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Calendars
+        public async Task<IActionResult> Index()
+        {
+            var bbContext = _context.Calendars.Include(c => c.Course);
+            return View(await bbContext.ToListAsync());
+        }
+
+        // GET: Calendars/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var calendar = await _context.Calendars
+                .Include(c => c.Course)
+                .FirstOrDefaultAsync(m => m.EventId == id);
+            if (calendar == null)
+            {
+                return NotFound();
+            }
+
+            return View(calendar);
+        }
+
+        // GET: Calendars/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId");
+            return View();
+        }
+
+        // POST: Calendars/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EventId,CourseId,EventDate,Type,EventName")] Calendar calendar)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(calendar);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId", calendar.CourseId);
+            return View(calendar);
+        }
+
+        // GET: Calendars/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var calendar = await _context.Calendars.FindAsync(id);
+            if (calendar == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId", calendar.CourseId);
+            return View(calendar);
+        }
+
+        // POST: Calendars/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EventId,CourseId,EventDate,Type,EventName")] Calendar calendar)
+        {
+            if (id != calendar.EventId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(calendar);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CalendarExists(calendar.EventId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId", calendar.CourseId);
+            return View(calendar);
+        }
+
+        // GET: Calendars/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var calendar = await _context.Calendars
+                .Include(c => c.Course)
+                .FirstOrDefaultAsync(m => m.EventId == id);
+            if (calendar == null)
+            {
+                return NotFound();
+            }
+
+            return View(calendar);
+        }
+
+        // POST: Calendars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var calendar = await _context.Calendars.FindAsync(id);
+            _context.Calendars.Remove(calendar);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CalendarExists(int id)
+        {
+            return _context.Calendars.Any(e => e.EventId == id);
+        }
+    }
+}
